2f7acae601da1e8a94bc8133d0849825694e0675
Sethricks340 <slricks340@byui.edu>
2026-04-08 18:11:06 -0600

    Dragging Icon Works Smoothly

--- MainNode.cs
+++ MainNode.cs
@@ -3,59 +3,45 @@ using System;
 
 public partial class MainNode : Node2D
 {
-	//GD.Print("Hello, world!");
 	private bool dragging = false;
 	private Vector2 offset;
-	private Node2D parentScript;
-	private Sprite2D _cat;
 	private Node window_inst;
-	
+
 	public override void _Ready()
 	{
-		//if ((GetParent() as Node2D) == null) GD.Print("parent is null");
-		_cat = GetNode<Sprite2D>("Cat");
-			
 		var window_script = (GDScript)GD.Load("res://window.gd");
 		window_inst = (Node)window_script.New();
 		AddChild(window_inst);
-
-		//window_inst.Call("MoveWindowTo", new Vector2(0, 0));
 	}
 
 	public override void _Input(InputEvent @event)
 	{
-		if (@event is InputEventMouseButton mouseEvent)
+		if (@event is InputEventMouseButton mouseEvent && mouseEvent.ButtonIndex == MouseButton.Left)
 		{
-			if (mouseEvent.ButtonIndex == MouseButton.Left)
+			if (mouseEvent.Pressed)
 			{
-				if (mouseEvent.Pressed)
+				Vector2 mousePos = DisplayServer.MouseGetPosition();
+				Rect2 globalRect = new Rect2((Vector2)window_inst.Call("get_window_position"), new Vector2(130, 130));
+
+				if (globalRect.HasPoint(mousePos))
 				{
-					// Check if mouse is over the sprite
-					Vector2 mousePos = GetGlobalMousePosition();
-					GD.Print("mousePos: " + mousePos);
-					Rect2 localRect = _cat.GetRect();            
-					Rect2 globalRect = new Rect2(_cat.GlobalPosition + localRect.Position, localRect.Size);
-					GD.Print("globalRect: " + globalRect);
-					
-					if (globalRect.HasPoint(mousePos))
-					{
-						dragging = true;
-						offset = (Vector2)window_inst.Call("get_window_position") - mousePos;
-					}
+					dragging = true;
+					offset = (Vector2)window_inst.Call("get_window_position") - mousePos;
 				}
-				else
-				{
-					dragging = false;
-				}
+			}
+			else
+			{
+				dragging = false;
 			}
 		}
 	}
 
 	public override void _Process(double delta)
-	{		
+	{
 		if (dragging)
 		{
-			window_inst.Call("MoveWindowTo", GetGlobalMousePosition() + offset);
+			Vector2 mousePos = DisplayServer.MouseGetPosition();
+			window_inst.Call("MoveWindowTo", mousePos + offset);
 		}
 	}
 }
